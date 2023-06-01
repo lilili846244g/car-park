290889ab22fb051eb34a7e2418bb446749929285
ly li  <lilili846244@gmail.com>
2023-06-01 18:15:19 +0800

    j

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,19 +6,19 @@ toggleButton.addEventListener('click', () => {
   gsap.to(".big-logo", 0.5,{
         y:"-10vh",
        width:"60%",
-       ease: 'easeOutExpo'
+       ease: 'easeIn'
        
     })
     gsap.to(".choose-rent-part",0.5, {
         y:"-14vh",
-        ease: 'easeOutExpo'
+        ease: 'easeIn'
       
     })
    
   
     gsap.to(quota,0.5, {
         y:"-14vh",
-        ease: 'easeOutExpo',
+        ease: 'easeIn',
         display: 'block'
     });
 });
