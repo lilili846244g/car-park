2f7d8767b746d4d26810aee84c3c80d8995cc193
ly li  <lilili846244@gmail.com>
2023-06-01 18:07:24 +0800

    v

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,22 @@ const quota = document.querySelector('.quota');
 
 toggleButton.addEventListener('click', () => {
 
-  gsap.to(".big-logo", {
+  gsap.to(".big-logo", 0.5,{
         y:"-10vh",
        width:"64%",
        ease: 'easeOutExpo'
        
     })
-    gsap.to(".choose-rent-part", {
+    gsap.to(".choose-rent-part",0.5, {
         y:"-14vh",
         ease: 'easeOutExpo'
       
     })
-    gsap.to(".quota-box", {
+   
+  
+    gsap.to(quota,0.5, {
         y:"-14vh",
-        ease: 'easeOutExpo'
-      
-    })
-    gsap.to(quota, {
+        ease: 'easeOutExpo',
         display: 'block'
     });
 });
